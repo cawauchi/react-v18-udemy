433a8ab427cbae3c06f15c93f122db6bfbab7c0c
cawauchi <tatsuya.kawauchi@photocreate.co.jp>
2022-10-05 14:20:42 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  console.log("appがレンダリングされた")
+  useEffect(() => {
+    console.log("useEffect")
+    // strictModeだとこれが必ずしも一回だけ実装されるわけではなくなる?
+  },[])
   return (
     <div className="App">
       <header className="App-header">
